1db80fa1b759455ab47b245d6ab32cf58148f4cd
caoxuehan <caoxuehan@qq.com>
2024-01-29 23:01:24 +0800

    用户中心前端+docker部署

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import { GithubOutlined } from '@ant-design/icons';
+import { DefaultFooter } from '@ant-design/pro-components';
+const Footer: React.FC = () => {
+  const defaultMessage = '曹雪晗出品';
+  const currentYear = new Date().getFullYear();
+  return (
+    <DefaultFooter
+      copyright={`${currentYear} ${defaultMessage}`}
+      links={[
+        {
+          key: 'github',
+          title: <GithubOutlined />,
+          href: 'https://github.com/gitgg021',
+          blankTarget: true,
+        },
+      ]}
+    />
+  );
+};
+export default Footer;
